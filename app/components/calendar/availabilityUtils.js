620e5d816f25750b29d27815e1dd6197cd89c639
Liam Hamill-Mamo <lhamillmamo@kings.edu.au>
2024-07-15 20:47:26 +1000

    Fixed bug with availability where event does not contain a tutor or event start time is the same time as availability

--- app/components/calendar/availabilityUtils.js
+++ app/components/calendar/availabilityUtils.js
@@ -10,28 +10,35 @@ export const splitAvailabilities = (availabilities, events) => {
     const tempSlots = [];
 
     events.forEach((event) => {
-      const eventStart = moment(event.start);
-      const eventEnd = moment(event.end);
+      // Check if the event's assigned tutor matches the availability's tutor
+      const isEventForTutor = event.staff.some(staff => staff.value === availability.tutor);
 
-      if (eventStart.isBetween(currentStart, currentEnd, null, '[)') || eventEnd.isBetween(currentStart, currentEnd, null, '[)') || (eventStart.isBefore(currentStart) && eventEnd.isAfter(currentEnd))) {
-        if (eventStart.isAfter(currentStart) && eventEnd.isBefore(currentEnd)) {
-          tempSlots.push({
-            ...availability,
-            start: currentStart.toDate(),
-            end: eventStart.toDate(),
-          });
-          currentStart = eventEnd;
-        } else if (eventStart.isBefore(currentStart) && eventEnd.isAfter(currentStart) && eventEnd.isBefore(currentEnd)) {
-          currentStart = eventEnd;
-        } else if (eventStart.isAfter(currentStart) && eventStart.isBefore(currentEnd)) {
-          tempSlots.push({
-            ...availability,
-            start: currentStart.toDate(),
-            end: eventStart.toDate(),
-          });
-          currentStart = eventEnd;
-        } else if (eventStart.isBefore(currentStart) && eventEnd.isAfter(currentEnd)) {
-          currentStart = currentEnd; // This will skip adding this slot as it is fully covered by an event
+      if (isEventForTutor) {
+        const eventStart = moment(event.start);
+        const eventEnd = moment(event.end);
+
+        if (eventStart.isBetween(currentStart, currentEnd, null, '[)') || eventEnd.isBetween(currentStart, currentEnd, null, '[)') || (eventStart.isBefore(currentStart) && eventEnd.isAfter(currentEnd))) {
+          if (eventStart.isSame(currentStart) && eventEnd.isBefore(currentEnd)) {
+            currentStart = eventEnd;
+          } else if (eventStart.isAfter(currentStart) && eventEnd.isBefore(currentEnd)) {
+            tempSlots.push({
+              ...availability,
+              start: currentStart.toDate(),
+              end: eventStart.toDate(),
+            });
+            currentStart = eventEnd;
+          } else if (eventStart.isBefore(currentStart) && eventEnd.isAfter(currentStart) && eventEnd.isBefore(currentEnd)) {
+            currentStart = eventEnd;
+          } else if (eventStart.isAfter(currentStart) && eventStart.isBefore(currentEnd)) {
+            tempSlots.push({
+              ...availability,
+              start: currentStart.toDate(),
+              end: eventStart.toDate(),
+            });
+            currentStart = eventEnd;
+          } else if (eventStart.isBefore(currentStart) && eventEnd.isAfter(currentEnd)) {
+            currentStart = currentEnd; // This will skip adding this slot as it is fully covered by an event
+          }
         }
       }
     });
