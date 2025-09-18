196fd11d69e01a64535f9f3c26da3684e2f2a61a
Matthew Mei <matthewmeim@gmail.com>
2025-09-18 14:39:08 +1000

    removed old route - couldnt use

--- app/api/send-event/route.js
+++ app/api/send-event/route.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 
 export async function POST(req) {
   try {
-    const { subject, eventId, start, end, attendees } = await req.json();
+    const { subject, description, eventId, start, end, attendees } = await req.json();
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
       port: 465,
@@ -13,14 +13,11 @@ export async function POST(req) {
       },
     });
 
-    const payload = {
-      subject,
-      start,
-      end,
-      eventId,
-      attendees,
-    };
+    const payload = { subject, description, start, end, eventId, attendees };
 
+    // in deployment - modify to computing@kings.edu.au 
+    // in dev - only works with mmei@kings.edu.au from MS Power Automate Configuration
+    // https://make.powerautomate.com/environments/Default-81a86f53-d361-4ab3-82fd-d98fac02520d/flows/7a8bc324-b1fa-4734-b2d7-51c745ec63cf/details
     const info = await transporter.sendMail({
       from: process.env.GMAIL_USER,
       to: "mmei@kings.edu.au",
