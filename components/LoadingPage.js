8ed6b19acd218bf3ab9865348490ba221a42c32a
Matthew Mei <matthewmeim@gmail.com>
2025-09-29 12:52:18 +1000

    loading page changes to loading dots

--- components/LoadingPage.js
+++ components/LoadingPage.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
 
 const LoadingPage = ({ withBackground = true }) => {
+  const [dotCount, setDotCount] = useState(1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDotCount(prevCount => {
+        const newCount = prevCount >= 4 ? 1 : prevCount + 1;
+        return newCount;
+      });
+    }, 500);
+
+    return () => {
+      console.log('Cleaning up interval');
+      clearInterval(interval);
+    };
+  }, []);
+
+  const dots = '.'.repeat(dotCount);
+  const backgroundStyle = 'tw-fixed tw-inset-0 tw-bg-gradient-to-r tw-from-indigo-500 tw-via-purple-500 tw-to-pink-500'
+  const textColor = withBackground ? 'text-white' : 'text-dark';
+
   return (
-    <div className={`tw-flex tw-items-center tw-justify-center ${withBackground ? 'tw-fixed tw-inset-0 tw-bg-gradient-to-r tw-from-indigo-500 tw-via-purple-500 tw-to-pink-500' : 'tw-w-full tw-h-full'}`}>
-      <div className="tw-flex tw-flex-col tw-items-center tw-justify-center">
-        <svg className={`tw-w-8 tw-h-8 ${withBackground ? 'tw-text-white' : 'tw-text-black'} tw-animate-spin`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-          <circle className="tw-opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-          <path className="tw-opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.963 7.963 0 014 12H0c0 2.21.896 4.21 2.343 5.657l1.414-1.414z"></path>
-        </svg>
-        <span className={`tw-text-lg tw-mt-2 ${withBackground ? 'tw-text-white' : 'tw-text-black'}`}>Loading...</span>
+    <div className={`d-flex align-items-center justify-content-center ${backgroundStyle}`}>
+      <div className="d-flex flex-column align-items-center justify-content-center">
+        <Spinner
+          animation="border"
+          variant={withBackground ? 'light' : 'dark'}
+          style={{ width: '2rem', height: '2rem' }}
+        />
+        <span className={`fs-5 mt-2 ${textColor}`}>Loading{dots}</span>
       </div>
     </div>
   );
