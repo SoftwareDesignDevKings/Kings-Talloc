b8c17f6a5b26dbdf415a68c4edabe66d6d559579
Matthew Mei <matthewmeim@gmail.com>
2025-10-04 00:48:45 +1000

    fixed auth not changing after initial loading page return

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -3,6 +3,7 @@
 import LoadingPage from '@/components/LoadingPage.jsx';
 import LoginPage from '@/components/LoginPage.jsx';
 import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react'
 
 /**
  * Authentication provider to wrap around components that require authentication.
@@ -10,10 +11,20 @@ import { useSession } from 'next-auth/react';
  * @returns 
  */
 const AuthProvider = ({ children }) => {
-    const sessionObj = useSession();
-    if (sessionObj.status === "loading") return <LoadingPage />;
-    if (sessionObj.status === "unauthenticated") return <LoginPage />;
     
+    const sessionObj = useSession();
+    const [isLoading, setIsLoading] = useState(true);    
+
+    useEffect(() => {
+        if (sessionObj.status === "loading") {
+            setIsLoading(false)
+        }
+
+    }, [sessionObj.status])
+
+    if (isLoading) return <LoadingPage />;
+    if (sessionObj.status === "unauthenticated") return <LoginPage />;
+
     return (<>{children}</>);
 }
 
