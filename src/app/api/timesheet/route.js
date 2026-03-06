7b7033d0b51a392cf90fe5ac6e506deefb45b233
Matthew Mei <matthewmeim@gmail.com>
2026-03-07 01:01:38 +0800

    fix 2: fib inequality check

--- src/app/api/timesheet/route.js
+++ src/app/api/timesheet/route.js
@@ -28,7 +28,7 @@ async function fetchUserShifts(userEmail, startDateSyd, endDateSyd, timesheetTyp
     if (timesheetType == "coach") {
         query = query.where('workType', '==', 'coaching');
     } else {
-        query = query.where('workType', '==', 'tutoring');
+        query = query.where('workType', '!=', 'coaching');
     }
 
     query = query.where('start', '>=', startDateUTC)
