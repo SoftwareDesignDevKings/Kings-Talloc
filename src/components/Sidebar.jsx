57189fcf3211ad5bb131a5c6be27aa535efe71b7
Matthew Mei <matthewmeim@gmail.com>
2026-04-28 15:21:33 +0800

    feat: renamed sidebar 'User Roles' to 'Manage Users'

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -112,10 +112,10 @@ const Sidebar = ({ user, userRole }) => {
                             <li className={styles.navItem}>
                                 <Link
                                     href="/userRoles"
-                                    className={`${styles.navLink} ${isCollapsed ? styles.navLinkCollapsed : styles.navLinkExpanded} ${pathname.startsWith('/userRoles') ? styles.activeNavLink : ''}`}
+                                    className={`${styles.navLink} ${isCollapsed ? stylebrs.navLinkCollapsed : styles.navLinkExpanded} ${pathname.startsWith('/userRoles') ? styles.activeNavLink : ''}`}
                                 >
                                     <FiUsers className={styles.navIcon} />
-                                    <span className={styles.navLabel}>User Roles</span>
+                                    <span className={styles.navLabel}>Manage Users</span>
                                 </Link>
                             </li>
                         )}
